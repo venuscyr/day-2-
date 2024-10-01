221e7c1efea325d5bd442e06c6c4f1877ae3ece1
venuscyr <venus.amako@epitech.digital>
2024-10-01 11:06:05 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,19 @@
+function changebackground() {
+  var magicNumber = 1;
+  var magicNumber = 2;
+  var magicNumber = 3;
+
+  if (magicNumber === 3) {
+    paintRedRectangle();
+  }
+ if (magicNumber === 3) {
+    paintYellowRectangle();
+  }
+   if (magicNumber === 3) {
+    paintGreenRectangle();
+  }
+
+  
+}
+
+changebackground();
