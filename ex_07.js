7169c0fe557060885d79e60a780063e412cb015a
venuscyr <venus.amako@epitech.digital>
2024-10-01 18:12:17 +0200

    ex_07.js

---
+++ ex_07.js
@@ -0,0 +1,9 @@
+fruits = ["Blackberries", "Apple", "Banana", "Mango", "Peaches", "Kiwi"];
+
+function getFruitNameFromIndex(integer){
+
+return fruits 
+}
+
+ result = getFruitNameFromIndex("5")
+displayResult(result)
